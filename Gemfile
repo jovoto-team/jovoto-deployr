bcf0ce87102f7901c3a808b9d17744a5c5615aac
Alexander Presber <post@momolog.info>
2016-07-19 00:48:21 +0200

    start project

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://gemcutter.org"
+ruby '2.1.7'
+
+gem 'sinatra'
+gem 'aws-sdk', '~> 2'
---
+++ index.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'sinatra'
+require 'crack'
+require 'json'
+require 'aws-sdk'
+
+APPS = {
+  'develop' => 'e5bd6ab7-d55f-4123-b8d4-31cb288fd59e',
+  'release' => '3b5d8888-38a8-4145-8db7-29c5b2238d66'
+}
+
+class Dispatcher
+  def initialize
+    @opsworks = Aws::OpsWorks::Client.new
+  end
+
+  def go branch
+    workload = {
+      stack_id: '08fa10ff-dcda-45f8-afec-5b0348a49141',
+      app_id:   APPS[branch],
+      command:  {
+        name: 'deploy'
+      }
+    }
+    p workload
+    begin
+      result = @opsworks.create_deployment workload
+      p result
+      p "dispatched to '#{branch}'"
+    rescue Exception => e
+      p e.inspect
+      p e.backtrace
+    end
+  end
+end
+
+post '/' do
+  p "received post"
+  if (payload = params[:payload])
+    parsed = JSON::parse(payload)
+    puts parsed
+    ref = parsed['ref']
+    if branch = ref.gsub(%r{refs/heads/}, '')
+      p "-> rebuilding #{branch}"
+      Dispatcher.new.go branch
+    end
+  end
+end
